9fa14df4102ee35696aa8086d654e4d5356390f3
Owner <l2206693309as@gmail.com>
2024-07-06 14:50:53 +0900

    2回コミット

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,19 +1,46 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name="Kazuya Kakuta";
+	private String age="16";
+	private static Map<String,String> StudentMap = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
+		StudentMap.put("Kazuya","10");
 	}
-	@GetMapping("/example")
-	public String sample(){
-		return "\r\nエスケープ文字では改行されないが、<br>タグでは改行される";
+	@GetMapping("/studentInfo")
+	public String studentInfo(){
+		return name + " " + age + "歳";
+	}
+
+	@GetMapping("/StudentMap")
+	public String StudentMap(){return StudentMap.get(name);};
+
+	@PostMapping("/studentInfo")
+	public void studentInfo(String name,String age) {
+		this.name = name;
+		this.age = age;
+	}
+		@PostMapping("/name")
+	public void name(String name){
+		this.name=name;}
+
+	@PostMapping("/StudentMap")
+		public void StudentMap(String name,String age){
+		this.name=name;
+		this.age=age;
+		this.StudentMap.put(name,age);
 	}
 }
