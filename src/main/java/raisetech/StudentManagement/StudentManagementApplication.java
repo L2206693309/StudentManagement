82ce0be1dd8d0109d718000098f5b41e57a2f721
L2206693309 <l2206693309as@gmail.com>
2024-07-13 16:10:02 +0900

    Update StudentManagementApplication.java

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -12,38 +12,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagementApplication {
 
-	private String name="Kazuya Kakuta";
-	private String age="16";
-	private static Map<String,String> StudentMap = new HashMap<>();
+  private String name = "Kazuya Kakuta";
+  private String age = "16";
+  private static Map<String, String> StudentMap = new HashMap<>();
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-		StudentMap.put("Kazuya","10");
-	}
-	@GetMapping("/studentInfo")
-	public String studentInfo(){
-		return name + " " + age + "歳";
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+    StudentMap.put("Kazuya", "10");
+  }
 
-	@GetMapping("/StudentMap")
-	public String StudentMap(){return StudentMap.get(name);};
+  @GetMapping("/studentInfo")
+  public String studentInfo() {
+    return name + " " + age + "歳";
+  }
 
-	@GetMapping("/name")
-	public String name(){System.out.println(name); return this.name;};
+  @GetMapping("/StudentMap")
+  public String StudentMap() {
+    return StudentMap.get(name);
+  }
 
-	@PostMapping("/studentInfo")
-	public void studentInfo(String name,String age) {
-		this.name = name;
-		this.age = age;
-	}
-		@PostMapping("/name")
-	public void name(String name){
-		this.name=name;}
+  @GetMapping("/name")
+  public String name() {
+    System.out.println(name);
+    return this.name;
+  }
 
-	@PostMapping("/StudentMap")
-		public void StudentMap(String name,String age){
-		this.name=name;
-		this.age=age;
-		this.StudentMap.put(name,age);
-	}
+  @PostMapping("/studentInfo")
+  public void studentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @PostMapping("/name")
+  public void name(String name) {
+    this.name = name;
+  }
+
+  @PostMapping("/StudentMap")
+  public void StudentMap(String name, String age) {
+    this.name = name;
+    this.age = age;
+    this.StudentMap.put(name, age);
+  }
 }
