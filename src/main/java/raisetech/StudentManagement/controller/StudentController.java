7753a4239c7f65b6bdea37c7f9fa7b77392c2760
Owner <l2206693309as@gmail.com>
2024-08-17 15:53:50 +0900

    10

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,7 @@ import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private String name = "Kazuya Kakuta";
@@ -23,18 +25,18 @@ public class StudentController {
   private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service,StudentConverter converter) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
     this.converter = converter;
   }
 
   @GetMapping("/students")
-  public List<StudentDetail> students() {
+  public String students(Model model) {
     List<Students> students1 = service.students();
     List<StudentsCourses> studentsCourses = service.studentsCourses();
-    return converter.convertStudentDetails(students1, studentsCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students1, studentsCourses));
+    return "studentList";
   }
-
 
 
   @GetMapping("/students30")
