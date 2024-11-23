f570377beb673a04d0deb733ebd967b16aa63ce0
Owner <l2206693309as@gmail.com>
2024-11-23 16:19:00 +0900

    28回コミット add リポジトリの自動テスト作成 MybatisTestによるテスト用DBの定義を作成 StudentRepositoryに定義されている全てのメソッドの自動テスト作成

---
+++ src/test/java/raisetech/StudentManagement/repository/StudentRepositoryTest.java
@@ -0,0 +1,134 @@
+package raisetech.StudentManagement.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import raisetech.StudentManagement.data.StudentCourses;
+import raisetech.StudentManagement.data.Students;
+
+@MybatisTest
+class StudentRepositoryTest {
+
+  @Autowired
+  private StudentRepository sut;
+
+  @Test
+  void 受講生の全件検索が行えること(){
+    List<Students> actual = sut.findAllStudents();
+    Assertions.assertThat(actual.size()).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生の単一検索が行えること(){
+    Students actual = sut.searchStudent(1);
+    Assertions.assertThat(actual.getName()).isEqualTo("name1");
+  }
+
+  @Test
+  void 論理削除されていない受講生の検索が行えること(){
+    List<Students> actual = sut.searchUndeletedStudents();
+    Assertions.assertThat(actual.size()).isEqualTo(3);
+  }
+
+  @Test
+  void 受講生コース情報の全件検索が行えること(){
+    List<StudentCourses> actual = sut.findAllStudentCourseList();
+    Assertions.assertThat(actual.size()).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生コース情報の単一検索が行えること(){
+    List<StudentCourses> actual = sut.searchStudentCourse(1);
+    Assertions.assertThat(actual.size()).isEqualTo(1);
+    Assertions.assertThat(actual.getFirst().getCourseName()).isEqualTo("courseName1");
+  }
+
+  @Test
+  void 受講生テーブルのidの最大値の検索が行えること(){
+    Integer actual = sut.searchStudentsMaxId();
+    Assertions.assertThat(actual).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生コース情報テーブルのidの最大値の検索が行えること(){
+    Integer actual = sut.searchStudentsCoursesMaxId();
+    Assertions.assertThat(actual).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生の登録が行えること(){
+    Students student = new Students();
+    student.setId(6);
+    student.setName("testName");
+    student.setNickname("testNickname");
+    student.setAge(2887);
+    student.setGender("testGender");
+    student.setFurigana("testFurigana");
+    student.setLivingArea("testLivingArea");
+    student.setMailAddress("testMailAddress");
+    student.setRemark("testRemark");
+    student.setIsDeleted(false);
+
+    sut.registerStudent(student);
+
+    List<Students> actual = sut.findAllStudents();
+
+    Assertions.assertThat(actual.size()).isEqualTo(6);
+  }
+
+  @Test
+  void 受講生コース情報の登録が行えること(){
+    StudentCourses studentCourse = new StudentCourses();
+    studentCourse.setCourseName("testCourseName");
+    studentCourse.setId(79);
+    studentCourse.setStartDate(LocalDate.now());
+    studentCourse.setEndDate(LocalDate.now().plusYears(1));
+    studentCourse.setSId(99822);
+
+    sut.registerStudentCourse(studentCourse);
+
+    List<StudentCourses> actual = sut.findAllStudentCourseList();
+
+    Assertions.assertThat(actual.size()).isEqualTo(6);
+  }
+
+  @Test
+  void 受講生の更新が行えること(){
+    Students student = new Students();
+    student.setId(1);
+    student.setName("testName");
+    student.setNickname("testNickname");
+    student.setAge(2887);
+    student.setGender("testGender");
+    student.setFurigana("testFurigana");
+    student.setLivingArea("testLivingArea");
+    student.setMailAddress("testMailAddress");
+    student.setRemark("testRemark");
+    student.setIsDeleted(false);
+
+    sut.updateStudent(student);
+
+    Students actual = sut.searchStudent(1);
+
+    Assertions.assertThat(actual.getName()).isEqualTo("testName");
+  }
+
+  @Test
+  void 受講生コース情報の更新が行えること(){
+    StudentCourses studentCourse = new StudentCourses();
+    studentCourse.setCourseName("testCourseName");
+    studentCourse.setId(1);
+    studentCourse.setStartDate(LocalDate.now());
+    studentCourse.setEndDate(LocalDate.now().plusYears(1));
+    studentCourse.setSId(99822);
+
+    sut.updateStudentCourse(studentCourse);
+
+    List<StudentCourses> actual = sut.searchStudentCourse(1);
+
+    Assertions.assertThat(actual.getFirst().getCourseName()).isEqualTo("testCourseName");
+  }
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO students VALUES
+(1, 'name1', 'furigana1', 'nickname1', 'mailAddress1', 'livingArea1', 1, 'gender1', 'remark1', 0),
+(2, 'name2', 'furigana2', 'nickname2', 'mailAddress2', 'livingArea2', 2, 'gender2', 'remark2', 0),
+(3, 'name3', 'furigana3', 'nickname3', 'mailAddress3', 'livingArea3', 3, 'gender3', 'remark3', 0),
+(4, 'name4', 'furigana4', 'nickname4', 'mailAddress4', 'livingArea4', 4, 'gender4', 'remark4', 1),
+(5, 'name5', 'furigana5', 'nickname5', 'mailAddress5', 'livingArea5', 5, 'gender5', 'remark5', 1);
+
+INSERT INTO students_courses VALUES
+(1, 1, 'courseName1', '2024-01-01', '2025-01-01'),
+(2, 2, 'courseName2', '2024-01-02', '2025-01-02'),
+(3, 3, 'courseName3', '2024-01-03', '2025-01-03'),
+(4, 4, 'courseName4', '2024-01-04', '2025-01-04'),
+(5, 5, 'courseName5', '2024-01-05', '2025-01-05');
